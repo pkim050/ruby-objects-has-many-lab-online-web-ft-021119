44c766db7be3ac412d91ae498641093dc5fbdbf7
Patrick Kim <pkim050@gmail.com>
2019-02-25 23:02:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,5 +11,8 @@ class Author
     class_post.author = self
   end
   
-  #def add_post_by_
+  def add_post_by_title(post_title)
+    class_post = Post.new(post_title)
+    @posts << class_post
+    class_post.author = self
 end
