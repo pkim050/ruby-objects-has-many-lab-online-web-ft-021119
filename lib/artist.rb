f8974a7eb191bfd89cad695044cb970b2e798c83
Patrick Kim <pkim050@gmail.com>
2019-02-25 22:55:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Artist
   attr_accessor :name, :songs
+  @@count = 0
   
   def initialize(name)
     @name = name
@@ -9,12 +10,14 @@ class Artist
   
   def add_song(class_song)
     @songs << class_song
+    count += 1
     class_song.artist = self
   end
   
   def add_song_by_name(song_name)
     class_song = Song.new(song_name)
     @songs << class_song
+    count += 1
     class_song.artist = self
   end
   
