9fbc1bb5969fc1a52d92ec5c2e657a9cb13a5684
Patrick Kim <pkim050@gmail.com>
2019-02-25 23:11:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :posts
+  @@count = 0
   
   def initialize(name)
     @name = name
@@ -8,12 +9,18 @@ class Author
   
   def add_post(class_post)
     @posts << class_post
+    @@count += 1
     class_post.author = self
   end
   
   def add_post_by_title(post_title)
     class_post = Post.new(post_title)
+    @@count += 1
     @posts << class_post
     class_post.author = self
+  end
+  
+  def self.post_count
+    @@count
   end
 end
