e89a855b0e5b7999dc7c6b8d4ef8bcc2ab277c9f
Patrick Kim <pkim050@gmail.com>
2019-02-25 22:37:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,4 @@
 require 'pry'
 class Artist
-  attr_accessor :name, :songs
   
-  def initialize(name) # Name is the song of a name from the argument and creates and empty array called songs
-    @name = name
-    @songs = []
-  end
-  
-  def add_song(name) # Adds a song by name, push back the name of the song to an array. song_name.artist = Artist
-    #binding.pry
-    name.artist = self
-    @songs << name
-  end
-  
-  def add_song_by_name(name) # Create a new class Song with a song name. Push back the class Song onto the array. Class song.artist = Artist
-    song = Song.new(name)
-    song.artist = self
-    @songs << song
-  end
-  
-  def self.song_count
-    count = 0
-    #binding.pry
-    @songs.each {|element| count += 1}
-  end
 end
